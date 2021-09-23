9fa9bc90c688489fa4d8b6b1afccddfe24be4268
David Mikaczo <nevermind5214@gmail.com>
2021-09-23 12:19:09 +0200

    Added som very NOT FINAL buttons

--- src/SzoftMod/Form1.h
+++ src/SzoftMod/Form1.h
@@ -45,13 +45,25 @@ namespace CppCLRWinformsProjekt {
 	protected:
 
 	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::Button^ button5;
+	private: System::Windows::Forms::Button^ button6;
+	private: System::Windows::Forms::Button^ button7;
+	private: System::Windows::Forms::Button^ button8;
+	private: System::Windows::Forms::Button^ button9;
+	private: System::Windows::Forms::Button^ button0;
+	private: System::Windows::Forms::Button^ buttonPlus;
+	private: System::Windows::Forms::Button^ buttonMinus;
 	protected:
 
 	private:
 		/// <summary>
 		/// Erforderliche Designervariable.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+		System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -62,11 +74,23 @@ namespace CppCLRWinformsProjekt {
 		{
 			this->buttonEquals = (gcnew System::Windows::Forms::Button());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->button6 = (gcnew System::Windows::Forms::Button());
+			this->button7 = (gcnew System::Windows::Forms::Button());
+			this->button8 = (gcnew System::Windows::Forms::Button());
+			this->button9 = (gcnew System::Windows::Forms::Button());
+			this->button0 = (gcnew System::Windows::Forms::Button());
+			this->buttonPlus = (gcnew System::Windows::Forms::Button());
+			this->buttonMinus = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// buttonEquals
 			// 
-			this->buttonEquals->Location = System::Drawing::Point(69, 158);
+			this->buttonEquals->Location = System::Drawing::Point(12, 226);
 			this->buttonEquals->Name = L"buttonEquals";
 			this->buttonEquals->Size = System::Drawing::Size(75, 23);
 			this->buttonEquals->TabIndex = 0;
@@ -76,16 +100,149 @@ namespace CppCLRWinformsProjekt {
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(44, 96);
+			this->textBox1->Location = System::Drawing::Point(12, 12);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(100, 20);
+			this->textBox1->Size = System::Drawing::Size(260, 20);
 			this->textBox1->TabIndex = 1;
+			this->textBox1->TextAlign = System::Windows::Forms::HorizontalAlignment::Right;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(12, 68);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 2;
+			this->button1->Text = L"1";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(93, 68);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->TabIndex = 3;
+			this->button2->Text = L"2";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &Form1::button2_Click);
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(174, 68);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(75, 23);
+			this->button3->TabIndex = 4;
+			this->button3->Text = L"3";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &Form1::button3_Click);
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(12, 97);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(75, 23);
+			this->button4->TabIndex = 5;
+			this->button4->Text = L"4";
+			this->button4->UseVisualStyleBackColor = true;
+			this->button4->Click += gcnew System::EventHandler(this, &Form1::button4_Click);
+			// 
+			// button5
+			// 
+			this->button5->Location = System::Drawing::Point(93, 97);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(75, 23);
+			this->button5->TabIndex = 6;
+			this->button5->Text = L"5";
+			this->button5->UseVisualStyleBackColor = true;
+			this->button5->Click += gcnew System::EventHandler(this, &Form1::button5_Click);
+			// 
+			// button6
+			// 
+			this->button6->Location = System::Drawing::Point(174, 97);
+			this->button6->Name = L"button6";
+			this->button6->Size = System::Drawing::Size(75, 23);
+			this->button6->TabIndex = 7;
+			this->button6->Text = L"6";
+			this->button6->UseVisualStyleBackColor = true;
+			this->button6->Click += gcnew System::EventHandler(this, &Form1::button6_Click);
+			// 
+			// button7
+			// 
+			this->button7->Location = System::Drawing::Point(12, 126);
+			this->button7->Name = L"button7";
+			this->button7->Size = System::Drawing::Size(75, 23);
+			this->button7->TabIndex = 8;
+			this->button7->Text = L"7";
+			this->button7->UseVisualStyleBackColor = true;
+			this->button7->Click += gcnew System::EventHandler(this, &Form1::button7_Click);
+			// 
+			// button8
+			// 
+			this->button8->Location = System::Drawing::Point(93, 126);
+			this->button8->Name = L"button8";
+			this->button8->Size = System::Drawing::Size(75, 23);
+			this->button8->TabIndex = 9;
+			this->button8->Text = L"8";
+			this->button8->UseVisualStyleBackColor = true;
+			this->button8->Click += gcnew System::EventHandler(this, &Form1::button8_Click);
+			// 
+			// button9
+			// 
+			this->button9->Location = System::Drawing::Point(174, 126);
+			this->button9->Name = L"button9";
+			this->button9->Size = System::Drawing::Size(75, 23);
+			this->button9->TabIndex = 10;
+			this->button9->Text = L"9";
+			this->button9->UseVisualStyleBackColor = true;
+			this->button9->Click += gcnew System::EventHandler(this, &Form1::button9_Click);
+			// 
+			// button0
+			// 
+			this->button0->Location = System::Drawing::Point(12, 155);
+			this->button0->Name = L"button0";
+			this->button0->Size = System::Drawing::Size(75, 23);
+			this->button0->TabIndex = 11;
+			this->button0->Text = L"0";
+			this->button0->UseVisualStyleBackColor = true;
+			this->button0->Click += gcnew System::EventHandler(this, &Form1::button0_Click);
+			// 
+			// buttonPlus
+			// 
+			this->buttonPlus->Location = System::Drawing::Point(93, 155);
+			this->buttonPlus->Name = L"buttonPlus";
+			this->buttonPlus->Size = System::Drawing::Size(75, 23);
+			this->buttonPlus->TabIndex = 12;
+			this->buttonPlus->Text = L"+";
+			this->buttonPlus->UseVisualStyleBackColor = true;
+			this->buttonPlus->Click += gcnew System::EventHandler(this, &Form1::buttonPlus_Click);
+			// 
+			// buttonMinus
+			// 
+			this->buttonMinus->Location = System::Drawing::Point(174, 155);
+			this->buttonMinus->Name = L"buttonMinus";
+			this->buttonMinus->Size = System::Drawing::Size(75, 23);
+			this->buttonMinus->TabIndex = 13;
+			this->buttonMinus->Text = L"-";
+			this->buttonMinus->UseVisualStyleBackColor = true;
+			this->buttonMinus->Click += gcnew System::EventHandler(this, &Form1::buttonMinus_Click);
 			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(284, 261);
+			this->Controls->Add(this->buttonMinus);
+			this->Controls->Add(this->buttonPlus);
+			this->Controls->Add(this->button0);
+			this->Controls->Add(this->button9);
+			this->Controls->Add(this->button8);
+			this->Controls->Add(this->button7);
+			this->Controls->Add(this->button6);
+			this->Controls->Add(this->button5);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
 			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->buttonEquals);
 			this->Name = L"Form1";
@@ -102,5 +259,41 @@ namespace CppCLRWinformsProjekt {
 		this->textBox1->Text = calculate(data).ToString();
 
 	}
-	};
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->textBox1->Text = this->textBox1->Text + "1";
+	}
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->textBox1->Text = this->textBox1->Text + "2";
+	}
+	private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->textBox1->Text = this->textBox1->Text + "3";
+	}
+	private: System::Void button4_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->textBox1->Text = this->textBox1->Text + "4";
+	}
+	private: System::Void button5_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->textBox1->Text = this->textBox1->Text + "5";
+	}
+	private: System::Void button6_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->textBox1->Text = this->textBox1->Text + "6";
+	}
+	private: System::Void button7_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->textBox1->Text = this->textBox1->Text + "7";
+	}
+	private: System::Void button8_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->textBox1->Text = this->textBox1->Text + "8";
+	}
+	private: System::Void button9_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->textBox1->Text = this->textBox1->Text + "9";
+	}
+	private: System::Void button0_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->textBox1->Text = this->textBox1->Text + "0";
+	}
+	private: System::Void buttonPlus_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->textBox1->Text = this->textBox1->Text + "+";
+	}
+	private: System::Void buttonMinus_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->textBox1->Text = this->textBox1->Text + "-";
+	}
+};
 }
