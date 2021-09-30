dee721c171b45acc0fe03e2b6b8949e90e81efeb
David Mikaczo <nevermind5214@gmail.com>
2021-09-30 15:07:19 +0200

    Added command line param support for testing

--- testingsrc/testing/main.cpp
+++ testingsrc/testing/main.cpp
@@ -3,33 +3,54 @@
 #include <vector>
 #include "../../src/SzoftMod/calcmodule.h"
 
-int main()
+int main(int argc, char** argv)
 {
     #ifdef _WIN32
     system("chcp 65001");//ékezetek FIX windowson
     system("cls");
     #endif
 
-    std::string str;
-    while(std::getline(std::cin, str))
+    if(argc > 2) std::cout<<"Túl sok a paraméter"<<std::endl;
+
+    else if(argc == 2) //ha 1 darab paraméter van
     {
-        try {
+        try{
             std::vector<char> inVectr;
-            for(char myChar : str)
+            std::string myArg = argv[1];
+            for(char MyChar : myArg)
             {
-                inVectr.push_back(myChar);
+                inVectr.push_back(MyChar);
             }
-            std::cout<<"Eredmény= "<<calculate(inVectr)<<std::endl;
-        }
-        catch (std::string errorText) {
-            std::cout<<std::endl<<"Error: "<<errorText<<std::endl;
+            std::cout<<calculate(inVectr)<<std::endl;
         }
         catch (const char* err) {
-            std::cout<<std::endl<<"Error: "<<err<<std::endl;
+            std::cout<<"Error: "<<err<<std::endl;
         }
-        catch (std::vector<char> errorVect) {
-            std::string koztesString(errorVect.begin(), errorVect.end());
-            std::cout<<std::endl<<"Error: "<<koztesString<<std::endl;
+    }
+
+    else // ha nincs paraméter akkor végtelen olvas
+    {
+        std::string str;
+        while(std::getline(std::cin, str))
+        {
+            try {
+                std::vector<char> inVectr;
+                for(char myChar : str)
+                {
+                    inVectr.push_back(myChar);
+                }
+                std::cout<<"Eredmény= "<<calculate(inVectr)<<std::endl;
+            }
+            catch (std::string errorText) {
+                std::cout<<std::endl<<"Error: "<<errorText<<std::endl;
+            }
+            catch (const char* err) {
+                std::cout<<std::endl<<"Error: "<<err<<std::endl;
+            }
+            catch (std::vector<char> errorVect) {
+                std::string koztesString(errorVect.begin(), errorVect.end());
+                std::cout<<std::endl<<"Error: "<<koztesString<<std::endl;
+            }
         }
     }
     return 0;
